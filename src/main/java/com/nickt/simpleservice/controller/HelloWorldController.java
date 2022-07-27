e9577341239384cec42231816bd7d12b371880c2
nickt <tl0321@126.com>
2022-07-27 16:16:05 +0800

    k8s

--- src/main/java/com/nickt/simpleservice/controller/HelloWorldController.java
+++ src/main/java/com/nickt/simpleservice/controller/HelloWorldController.java
@@ -3,14 +3,16 @@ package com.nickt.simpleservice.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.Date;
 
 @RestController
 public class HelloWorldController {
 
     @RequestMapping("/")
-    String helloWorld() {
-        return "Hello World! now(V2)=" + new Date().toLocaleString();
+    String helloWorld() throws UnknownHostException {
+        return "Hello World!("+ InetAddress.getLocalHost().getHostAddress()+") now(V2)=" + new Date().toLocaleString();
     }
 
 }
